a3491a502adb4bdfeba9cc831d09196f20d2784d
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2023-07-21 12:23:05 +0200

    Make fuzz target more interesting

--- src/test/java/org/example/ProtobufFuzzTests.java
+++ src/test/java/org/example/ProtobufFuzzTests.java
@@ -7,19 +7,27 @@ import com.google.protobuf.DescriptorProtos.FileDescriptorProto;
 import com.google.protobuf.Descriptors.DescriptorValidationException;
 import com.google.protobuf.Descriptors.FileDescriptor;
 import com.google.protobuf.DynamicMessage;
+import com.google.protobuf.InvalidProtocolBufferException;
 
 class ProtobufFuzzTests {
 
   @FuzzTest
-  void dynamicMessageFuzzTest(@NotNull DescriptorProto message) {
+  void dynamicMessageFuzzTest(@NotNull DescriptorProto messageType, byte @NotNull [] bytes) {
     FileDescriptorProto file = FileDescriptorProto.newBuilder()
         .setName("my_protos.proto")
-        .addMessageType(message)
+        .addMessageType(messageType)
         .build();
+    DynamicMessage message;
     try {
-      DynamicMessage.getDefaultInstance(FileDescriptor.buildFrom(file, new FileDescriptor[0])
-          .findMessageTypeByName(message.getName()));
+      message = DynamicMessage.getDefaultInstance(
+          FileDescriptor.buildFrom(file, new FileDescriptor[0])
+              .findMessageTypeByName(messageType.getName()));
     } catch (DescriptorValidationException ignored) {
+      return;
+    }
+    try {
+      message.getParserForType().parseFrom(bytes);
+    } catch (InvalidProtocolBufferException ignored) {
     }
   }
 }
